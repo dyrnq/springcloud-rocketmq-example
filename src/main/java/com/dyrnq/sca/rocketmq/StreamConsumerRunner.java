e4acd188f5bcddc07c574332681c39cd77b12ace
dyrnq <dyrnq@qq.com>
2024-10-15 10:52:44 +0800

    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;

--- src/main/java/com/dyrnq/sca/rocketmq/StreamConsumerRunner.java
+++ src/main/java/com/dyrnq/sca/rocketmq/StreamConsumerRunner.java
@@ -2,14 +2,13 @@ package com.dyrnq.sca.rocketmq;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import java.nio.charset.StandardCharsets;
 
 @Component
 @Slf4j
@@ -22,6 +21,17 @@ public class StreamConsumerRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("your_consumer_group");
         consumer.setNamesrvAddr(rocketmqNameSrv);
+
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);  //从最新的消息开始消费
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET_AND_FROM_MIN_WHEN_BOOT_FIRST);  //废弃，效果同上
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_MIN_OFFSET);  //废弃，效果同上
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_MAX_OFFSET);  //废弃 ，效果同上
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);  //从最早的消息开始消费
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_TIMESTAMP);  //从指定时间开始消费
+
+
+//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET); // 从哪个位置开始消费消息
+
         consumer.subscribe("demo", "*");
 
         consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
@@ -38,7 +48,8 @@ public class StreamConsumerRunner implements ApplicationRunner {
 
                 log.info("storeHost: {}, Broker: {} , Queue ID: {} , Message ID: {} , Received message: {}",  msg.getStoreHost().toString(), brokerName, queueId, messageId, messageBody);
             }
-            return null; // 返回消费状态
+//            return null; // 返回消费状态
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS; // 返回消费成功状态，自动提交
         });
 
         consumer.start();

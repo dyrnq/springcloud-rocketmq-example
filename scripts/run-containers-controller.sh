ddc0a42a84d9c8711af7cfc082ca2245cae7ac72
dyrnq <dyrnq@qq.com>
2024-10-17 08:30:11 +0800

    POC messageDelayLevel
    .setHeader(MessageConst.PROPERTY_DELAY_TIME_LEVEL, 19)

--- scripts/run-containers-controller.sh
+++ scripts/run-containers-controller.sh
@@ -110,6 +110,8 @@ brokerRole=ASYNC_MASTER
 flushDiskType=ASYNC_FLUSH
 waitTimeMillsInSendQueue=600
 brokerIP1=${ip4}
+#messageDelayLevel=1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+messageDelayLevel=1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h 20s
 EOF
 
 fastListenPort="10909"
